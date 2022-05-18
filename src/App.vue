6f5960715abfaef93b5da37ad1c192bee64b7244
caixiaocai19 <2945903048@qq.com>
2022-05-18 09:45:06 +0800

    feat:webpack的基础配置

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="son"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.son{
+    width: 100px;
+    height: 100px;
+    background-color:red;
+    margin:0 auto;
+}
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,35 @@
-const path  = require("path");
-module.exports={
-    entry: './path/to/my/entry/file.js',
-}
+const { resolve } = require("path");
+const {VueLoaderPlugin}  = require("vue-loader");
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    filename: "boundle.js"
+  },
+  module: {
+    rules: [
+      { test: /\.vue$/, use: "vue-loader" },
+      {
+        test: /\.s[ca]ss$/, 
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+      new VueLoaderPlugin()
+  ],
+};
